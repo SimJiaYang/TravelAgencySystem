99f26995aca1d489a091ac466e50457a565a2ae6
Louis <7jia1421@gmail.com>
2023-07-15 16:26:04 +0800

    Let the user determine whether to see the modified tour list or not.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -21,39 +22,60 @@ public class Main {
             System.out.println("\n" + tour.toString());
         }
 
-        System.out.println("-----------------------Modified Information----------------------------");
+        Scanner input = new Scanner(System.in);
+        boolean validInput = false;
+        int isModified = 0;
 
-        // Modified the tour information and pricing details
-        // For Overseas
-        overseasTour.getPeakPrice().setAdultPrice(2000);
-        overseasTour.getPeakPrice().setChildWithExtraBedPrice(2000);
-        overseasTour.getPeakPrice().setChildWithNoExtraBedPrice(2000);
-        overseasTour.getPeakPrice().setInfantPrice(2000);
-        overseasTour.getNormalPrice().setAdultPrice(1000);
-        overseasTour.getNormalPrice().setChildWithExtraBedPrice(1000);
-        overseasTour.getNormalPrice().setChildWithNoExtraBedPrice(1000);
-        overseasTour.getNormalPrice().setInfantPrice(1000);
-        ((OverseasTour)overseasTour).setState("BangKok");
-        ((OverseasTour)overseasTour).setCountry("Thailand");
-        ((OverseasTour)overseasTour).setDays(10);
-        ((OverseasTour)overseasTour).setJoinedTours(false);
+        while (!validInput){
+            try{
+                System.out.print("\n Do you want view the modified information? (1:YES Other:NO): ");
+                isModified = input.nextInt();
+                validInput = true;
+            }catch (Exception e){
+                System.out.println("\nInvalid input! Please enter a correct value. ");
+                input.nextLine();
+            }
+        }
 
-        // For domestic
-        domesticTour.getPeakPrice().setAdultPrice(3000);
-        domesticTour.getPeakPrice().setChildWithExtraBedPrice(3000);
-        domesticTour.getPeakPrice().setChildWithNoExtraBedPrice(3000);
-        domesticTour.getPeakPrice().setInfantPrice(3000);
-        domesticTour.getNormalPrice().setAdultPrice(1500);
-        domesticTour.getNormalPrice().setChildWithExtraBedPrice(1500);
-        domesticTour.getNormalPrice().setChildWithNoExtraBedPrice(1500);
-        domesticTour.getNormalPrice().setInfantPrice(1500);
-        ((DomesticTour)domesticTour).setState("Kuala Lumpur");
-        ((DomesticTour)domesticTour).setDays(9);
-        ((DomesticTour)domesticTour).setHoneymoonTours(true);
+        if(isModified == 1) {
+
+            System.out.println("\n-----------------------Modified Information----------------------------");
+
+            // Modified the tour information and pricing details
+            // For Overseas
+            overseasTour.getPeakPrice().setAdultPrice(2000);
+            overseasTour.getPeakPrice().setChildWithExtraBedPrice(2000);
+            overseasTour.getPeakPrice().setChildWithNoExtraBedPrice(2000);
+            overseasTour.getPeakPrice().setInfantPrice(2000);
+            overseasTour.getNormalPrice().setAdultPrice(1000);
+            overseasTour.getNormalPrice().setChildWithExtraBedPrice(1000);
+            overseasTour.getNormalPrice().setChildWithNoExtraBedPrice(1000);
+            overseasTour.getNormalPrice().setInfantPrice(1000);
+            overseasTour.setDays(10);
+            ((OverseasTour) overseasTour).setState("BangKok");
+            ((OverseasTour) overseasTour).setCountry("Thailand");
+            ((OverseasTour) overseasTour).setJoinedTours(false);
+
+            // For domestic
+            domesticTour.getPeakPrice().setAdultPrice(3000);
+            domesticTour.getPeakPrice().setChildWithExtraBedPrice(3000);
+            domesticTour.getPeakPrice().setChildWithNoExtraBedPrice(3000);
+            domesticTour.getPeakPrice().setInfantPrice(3000);
+            domesticTour.getNormalPrice().setAdultPrice(1500);
+            domesticTour.getNormalPrice().setChildWithExtraBedPrice(1500);
+            domesticTour.getNormalPrice().setChildWithNoExtraBedPrice(1500);
+            domesticTour.getNormalPrice().setInfantPrice(1500);
+            domesticTour.setDays(9);
+            ((DomesticTour) domesticTour).setState("Kuala Lumpur");
+            ((DomesticTour) domesticTour).setHoneymoonTours(true);
 
 
-        for(Tour tour : tourList){
-            System.out.println("\n" + tour.toString());
+            for (Tour tour : tourList) {
+                System.out.println("\n" + tour.toString());
+            }
+
+        }else{
+            System.exit(0);
         }
 
         }
